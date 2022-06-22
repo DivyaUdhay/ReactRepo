c2bb517a0afabc079efff27811eafc60e2d130a4
DivyaUdhay <divyaudhayaukmar.r@gmail.com>
2022-06-22 15:50:23 +0530

    firstcalass

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div>
+      <Message name="world"></Message>
+      <Message name="Mars"></Message>
+      <Message name="Moon"></Message>
+      <Message name="Jupiter"></Message>
+    </div>
+  );
+}
+
+export default App;
